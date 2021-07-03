e924cdc8e7363904fd569e5dba2b45cc6fc98172
remybeaudenon <remybeaudenon@yahoo.com>
2021-07-03 15:55:14 +0200

    test1

--- main.ts
+++ main.ts
@@ -2,6 +2,16 @@ basic.forever(function () {
     if (input.soundLevel() > 100) {
         basic.showIcon(IconNames.Chessboard)
     } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
         basic.clearScreen()
     }
+})
+basic.forever(function () {
+	
 })
