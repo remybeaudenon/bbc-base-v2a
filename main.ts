a5cf0fcd7dc199ea1989f5e1e3c3cfe7400091c0
remybeaudenon <remybeaudenon@yahoo.com>
2021-07-05 06:49:37 +0200

    Mise à jour pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -2,13 +2,6 @@ basic.forever(function () {
     if (input.soundLevel() > 100) {
         basic.showIcon(IconNames.Chessboard)
     } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
         basic.clearScreen()
     }
 })
